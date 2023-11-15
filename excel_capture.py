919d2389c6bc4591bd8672482db84677bb03a572
Mike Kroutikov <mkroutikov@innodata.com>
2023-11-15 08:23:28 -0500

    workaround failure to open filename with blanks

--- excel_capture.py
+++ excel_capture.py
@@ -1,4 +1,6 @@
 import os
+import tempfile
+import shutil
 from excel2img.excel2img import ExcelFile
 from PIL import ImageGrab
 
@@ -19,38 +21,44 @@ COPY_AS_BITMAP = 2
 
 def main(filename, fast=False):
 
-    name, _ = os.path.splitext(filename)
+    name, ext = os.path.splitext(filename)
 
-    with ExcelFile.open(filename) as excel:
-        num_sheets = excel.workbook.Sheets.Count
-        for i in range(1, num_sheets + 1):
-            sheet = excel.workbook.Sheets.Item(i)
-            print(f'Capturing {i}/{num_sheets}: {sheet.Name}')
-            last_used_column = sheet.UsedRange.Columns.Count
-            last_used_row = sheet.UsedRange.Rows.Count
+    with tempfile.TemporaryDirectory() as tdir:
 
-            print(f'\tlast_used_row={last_used_row}, last_used_column={last_used_column}')
-            if not fast:
-                while last_used_column > 1:
-                    if any(is_non_empty_cell(sheet.Cells(row, last_used_column)) for row in range(1, last_used_row + 1)):
-                        break
-                    last_used_column -= 1
-                print(f'\tafter shrinking columns: last_used_column={last_used_column}')
+        # Excel (or pywin32?) has problems opening filenames containing blanks. Workaround by copying to temp
+        tempname = f'{tdir}/workbook.{ext}'
+        shutil.copyfile(filename, tempname)
 
-                while last_used_row > 1:
-                    if any(is_non_empty_cell(sheet.Cells(last_used_row, col)) for col in range(1, last_used_column + 1)):
-                        break
-                    last_used_row -= 1
-                print(f'\tafter shrinking rows: last_used_row={last_used_row}')
+        with ExcelFile.open(tempname) as excel:
+            num_sheets = excel.workbook.Sheets.Count
+            for i in range(1, num_sheets + 1):
+                sheet = excel.workbook.Sheets.Item(i)
+                print(f'Capturing {i}/{num_sheets}: {sheet.Name}')
+                last_used_column = sheet.UsedRange.Columns.Count
+                last_used_row = sheet.UsedRange.Rows.Count
 
-            cell_range = f'A1:{column_index_to_address(last_used_column)}{last_used_row}'
-            print(f'\tcapturing cell range {cell_range}')
+                print(f'\tlast_used_row={last_used_row}, last_used_column={last_used_column}')
+                if not fast:
+                    while last_used_column > 1:
+                        if any(is_non_empty_cell(sheet.Cells(row, last_used_column)) for row in range(1, last_used_row + 1)):
+                            break
+                        last_used_column -= 1
+                    print(f'\tafter shrinking columns: last_used_column={last_used_column}')
 
-            sheet.Range(cell_range).CopyPicture(COPY_MODE_SCREEN, COPY_AS_BITMAP)
-            im = ImageGrab.grabclipboard()
-            img_filename = f'{name}-{i:02d}-{sheet.Name}.png'
-            im.save(img_filename, 'PNG')
-            print(f'\twritten sheet as {img_filename}')
+                    while last_used_row > 1:
+                        if any(is_non_empty_cell(sheet.Cells(last_used_row, col)) for col in range(1, last_used_column + 1)):
+                            break
+                        last_used_row -= 1
+                    print(f'\tafter shrinking rows: last_used_row={last_used_row}')
+
+                cell_range = f'A1:{column_index_to_address(last_used_column)}{last_used_row}'
+                print(f'\tcapturing cell range {cell_range}')
+
+                sheet.Range(cell_range).CopyPicture(COPY_MODE_SCREEN, COPY_AS_BITMAP)
+                im = ImageGrab.grabclipboard()
+                img_filename = f'{name}-{i:02d}-{sheet.Name}.png'
+                im.save(img_filename, 'PNG')
+                print(f'\twritten sheet as {img_filename}')
 
 
 if __name__ == '__main__':
